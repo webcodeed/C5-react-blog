b64df18b0f3cb4cd8389b70a891bd6ec436c71cc
ayo_ote <otegbadee@gmail.com>
2025-07-07 17:44:54 +0100

    added a delete button

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,35 +1,43 @@
-import { useState } from "react";
+import { useState } from "react"
 
 const Todo = () => {
-  const [list, setList] = useState([{ id: 1, desc: "Laundry" }]);
+    const [list, setList] = useState([{ id: 1, desc: "Laundry" }])
 
-  const [text, setText] = useState("");
-  return (
-    <>
-      <h2>Todo List</h2>
-      <ul>
-        {list.map((item) => (
-          <li key={item.id}>{item.desc}</li>
-        ))}
-      </ul>
-      <input
-        type="text"
-        placeholder="Add Input"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      />
-      <button
-        onClick={() => {
-          const id = crypto.randomUUID();
-          setList([...list, { id, desc: text }]);
-          setText("");
-          console.log(list);
-        }}
-      >
-        Submit
-      </button>
-    </>
-  );
-};
+    const [text, setText] = useState("")
+    return (
+        <>
+            <h2>Todo List</h2>
+            <ul>
+                {list.map((item) => (
+                    <li key={item.id}>
+                        {item.desc}{" "}
+                        <button
+                            onClick={() =>
+                                setList(list.filter((i) => i.id !== item.id))
+                            }
+                        >
+                            Delete
+                        </button>
+                    </li>
+                ))}
+            </ul>
+            <input
+                type="text"
+                placeholder="Add Input"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+            />
+            <button
+                onClick={() => {
+                    const id = crypto.randomUUID()
+                    setList([...list, { id, desc: text }])
+                    setText("")
+                }}
+            >
+                Submit
+            </button>
+        </>
+    )
+}
 
-export default Todo;
+export default Todo
